ce1614a58cfb4c1b2d2f2b3830cc8a4ddcdeb6cd
snetsplit <snetsplit@gmail.com>
2015-10-23 23:07:04 -0400

    Compatibility update
    This change adds a mechanism to make Xhangouts UICallButtons work with multiple versions of hangouts by automatically finding the correct class objects through reflection.

--- app/src/main/java/com/versobit/kmark/xhangouts/mods/UiCallButtons.java
+++ app/src/main/java/com/versobit/kmark/xhangouts/mods/UiCallButtons.java
@@ -19,6 +19,7 @@
 
 package com.versobit.kmark.xhangouts.mods;
 
+import android.app.Activity;
 import android.app.AndroidAppHelper;
 import android.content.res.Resources;
 import android.view.Menu;
@@ -28,6 +29,7 @@ import com.versobit.kmark.xhangouts.Config;
 import com.versobit.kmark.xhangouts.Module;
 
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.IXUnhook;
 
 import static com.versobit.kmark.xhangouts.XHangouts.HANGOUTS_RES_PKG_NAME;
@@ -36,7 +38,8 @@ import static de.robv.android.xposed.XposedHelpers.findClass;
 
 public final class UiCallButtons extends Module {
 
-    private static final String HANGOUTS_ACT_CONVERSATION_SUPER = "bhz";
+    private static final boolean checkForClass = true;
+    private static final String HANGOUTS_ACT_CONVERSATION_SUPER = "bka";
     private static final String HANGOUTS_ACT_CONVERSATION_SUPER_OPOM = "onPrepareOptionsMenu";
 
     private static final String HANGOUTS_MENU_CONVO_CALL = "realtimechat_conversation_call_menu_item";
@@ -53,44 +56,92 @@ public final class UiCallButtons extends Module {
         super(UiCallButtons.class.getSimpleName(), config);
     }
 
+    private String checkIfValidActivity(String className, ClassLoader loader) {
+        //check to see if class exists, if so, then if it is in an Activity, and finally if it has a valid method for menus
+        //if all three things are true then return the class name or else retrn an empty string
+        try {
+            Class classTest = findClass(className, loader);
+            if (Activity.class.isAssignableFrom(classTest)) {
+                try {
+                    findAndHookMethod(classTest, HANGOUTS_ACT_CONVERSATION_SUPER_OPOM, Menu.class, onPrepareOptionsMenu);
+                    debug("success: " + className);
+                    return("," + className);
+                } catch (NoSuchMethodError fail) {
+                    debug("fail: " + className);
+                }
+            }
+        } catch (XposedHelpers.ClassNotFoundError ignored) {
+        }
+        return "";
+    }
+
     @Override
     public IXUnhook[] hook(ClassLoader loader) {
-        Class cConversationActSuper = findClass(HANGOUTS_ACT_CONVERSATION_SUPER, loader);
+        //use the auto adapt code or not
+        if (checkForClass) {
 
-        return new IXUnhook[] {
-                findAndHookMethod(cConversationActSuper, HANGOUTS_ACT_CONVERSATION_SUPER_OPOM,
-                        Menu.class, onPrepareOptionsMenu)
-        };
+            String classList = "";
+
+            //the Obtusificator uses a 1-3 letter pattern for class names, so we just check every possible combo, and record it if it's good
+            for (char alphabet1 = 'a'; alphabet1 <= 'z'; alphabet1++) {
+                for (char alphabet2 = 'a'; alphabet2 <= 'z'; alphabet2++) {
+                    for (char alphabet3 = 'a'; alphabet3 <= 'z'; alphabet3++) {
+                        classList += checkIfValidActivity(String.valueOf(alphabet1) + String.valueOf(alphabet2) + String.valueOf(alphabet3), loader);
+                    }
+                    classList += checkIfValidActivity(String.valueOf(alphabet1) + String.valueOf(alphabet2), loader);
+                }
+                classList += checkIfValidActivity(String.valueOf(alphabet1), loader);
+            }
+
+            String[] activityClasses = classList.substring(1).split(",");
+
+            //convert the list of valid classes into method hooks
+            XC_MethodHook.Unhook[] hooks = new XC_MethodHook.Unhook[activityClasses.length];
+            for (int classIndex = 0; classIndex < activityClasses.length; classIndex++) {
+                hooks[classIndex] = findAndHookMethod(findClass(activityClasses[classIndex], loader), HANGOUTS_ACT_CONVERSATION_SUPER_OPOM, Menu.class, onPrepareOptionsMenu);
+            }
+
+            return hooks;
+        } else {
+
+            Class cConversationActSuper = findClass(HANGOUTS_ACT_CONVERSATION_SUPER, loader);
+
+
+            return new IXUnhook[]{
+                    findAndHookMethod(cConversationActSuper, HANGOUTS_ACT_CONVERSATION_SUPER_OPOM, Menu.class, onPrepareOptionsMenu)
+            };
+        }
     }
 
     private final XC_MethodHook onPrepareOptionsMenu = new XC_MethodHook() {
         @Override
         protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-            if(!config.modEnabled) {
+
+            if (!config.modEnabled) {
                 return;
             }
 
             debug(String.valueOf(config.hideCallButtons));
 
-            if(!config.hideCallButtons) {
+            if (!config.hideCallButtons) {
                 return;
             }
 
-            if(menuItemCallResId == RES_ID_UNSET || menuItemVideoCallResId == RES_ID_UNSET) {
+            if (menuItemCallResId == RES_ID_UNSET || menuItemVideoCallResId == RES_ID_UNSET) {
                 Resources res = AndroidAppHelper.currentApplication().getResources();
                 menuItemCallResId = res.getIdentifier(HANGOUTS_MENU_CONVO_CALL, "id", HANGOUTS_RES_PKG_NAME);
                 menuItemVideoCallResId = res.getIdentifier(HANGOUTS_MENU_CONVO_VIDEOCALL, "id", HANGOUTS_RES_PKG_NAME);
-                debug(String.format("Found convo menu item resource IDs: 0x%x, 0x%x",
-                        menuItemCallResId, menuItemVideoCallResId));
+                debug(String.format("Found convo menu item resource IDs: 0x%x, 0x%x", menuItemCallResId, menuItemVideoCallResId));
             }
+
             Menu menu = (Menu) param.args[0];
+
             for (int i = 0; i < menu.size(); i++) {
                 MenuItem item = menu.getItem(i);
-                if(item.getItemId() == menuItemCallResId || item.getItemId() == menuItemVideoCallResId) {
+                if (item.getItemId() == menuItemCallResId || item.getItemId() == menuItemVideoCallResId) {
                     item.setShowAsAction(MenuItem.SHOW_AS_ACTION_NEVER);
                 }
             }
         }
     };
-
 }
